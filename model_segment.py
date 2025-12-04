f42ac5b410e10d6cc8a71dce0f5c2c3549d0c34d
jiajun shen <shenjiajun21@mails.ucas.ac.cn>
2025-12-04 21:26:37 +0100

    features

--- model_segment.py
+++ model_segment.py
@@ -113,12 +113,7 @@ class FeatureExtractorSegment(FeatureExtractor):
     def get_page_features(self):
         pages_features = []
         page_targets = []
-        page_id = 0
         for page in self.loop_token_features():
-            page_id += 1
-            print("Processing page:", page_id)
-            if page_id > 10:
-                return pages_features, page_targets
             tokens = page.tokens
             if len(tokens) == 0:
                 continue
